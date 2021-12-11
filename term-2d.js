405b0c1bc7bab7b983d6a5c2e9137604cbadd82d
paritat221 <95929068+paritat221@users.noreply.github.com>
2021-12-11 12:14:30 +0100

    Delete term-2d.js

--- term-2d.js
+++
@@ -1,4 +0,0 @@
-exports.window = require("./window").window;
-exports.locate = require("./locate").locate;
-exports.getkey = require("./getkey").getkey;
-exports.clrText = require("./clrtext").clr_text;
